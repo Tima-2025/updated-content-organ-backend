f9033cb34a7fa5876eb69eaa23801dc712188258
Timatech <timaintegrated@gmail.com>
2025-09-19 00:12:11 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,12 +1,19 @@
- const { Pool } = require('pg');
- const fs = require('fs');
- const path = require('path');
- const pool = new Pool({ connectionString: process.env.DATABASE_URL });
- async function initDB() {
- 
-const initSql = fs.readFileSync(path.join(__dirname, '..', 'sql', 'init.sql'), 'utf8');
- // run init script once
- await pool.query(initSql);
- console.log('Initialized DB (tables & triggers)');
- }
- module.exports = { pool, initDB };
+const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,      // must match docker-compose service name 'db'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+async function initDB() {
+  const initSql = fs.readFileSync(path.join(__dirname, '..', 'sql', 'init.sql'), 'utf8');
+  await pool.query(initSql);  // run init script once
+  console.log('Initialized DB (tables & triggers)');
+}
+
+module.exports = { pool, initDB };
